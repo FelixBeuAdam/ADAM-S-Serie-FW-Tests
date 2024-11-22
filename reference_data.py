2968bf5c8726e55e775036a669c53ad1ca0e7b74
Felix Beu <felix.beu@adam-audio.de>
2024-11-22 13:32:14 +0100

    multichannel reference data measurement

--- reference_data.py
+++ reference_data.py
@@ -27,7 +27,7 @@ REF_DATA_PATH = os.path.join(FILE_PATH, 'Reference Data')
 AP_SEQ_FREQUENCY_RESPONSE_XLR = os.path.join(FILE_PATH, 'AP Sequences\\Frequency_Response_XLR.approjx')
 
 
-def measure_freq_res_xlr(path: str, channel: str):
+def measure_freq_res_xlr(path: str, channel:  list[str]):
     """
 
     :param channel:
@@ -39,19 +39,29 @@ def measure_freq_res_xlr(path: str, channel: str):
     APx.OpenProject(AP_SEQ_FREQUENCY_RESPONSE_XLR)
     # Run APx file
     APx.Sequence.Run()
+    # Channel Index
+    ch_idx = [InputChannelIndex.Ch1, InputChannelIndex.Ch2]
     # Save curve to file
-    name = 'Ref_Freq_Res_' + channel + '_dBFS.csv'
-    delay_name = 'Ref_Delay_' + channel + '_ms.csv'
-    file = os.path.join(path, name)
-    delay_file = os.path.join(path, delay_name)
-    APx.Sequence[0]['Acoustic Response'].SequenceResults['RMS Level'].ExportData(file)
-    delay = np.array(APx.Sequence[0]['Acoustic Response'].SequenceResults['Delay'].GetMeterValues()[0])
-    delay.tofile(delay_file, sep=';')
+    for i in range(len(channel)):
+        name = 'Ref_Freq_Res_' + channel[i] + '_dBFS.csv'
+        delay_name = 'Ref_Delay_' + channel[i] + '_ms.csv'
+        file = os.path.join(path, name)
+        delay_file = os.path.join(path, delay_name)
+        x_values = APx.Sequence[0]['Acoustic Response'].SequenceResults['RMS Level'].GetXValues(
+            ch_idx[i], VerticalAxis.Left, SourceDataType.Measured, 1)
+        y_values = APx.Sequence[0]['Acoustic Response'].SequenceResults['RMS Level'].GetYValues(
+            ch_idx[i], VerticalAxis.Left, SourceDataType.Measured, 1)
+        rms_level = np.array([x_values, y_values]).T
+        # Save Frequency Response to file
+        np.savetxt(file, rms_level, delimiter=';')
+        # Save Delay to file
+        delay = np.array(APx.Sequence[0]['Acoustic Response'].SequenceResults['Delay'].GetMeterValues()[i])
+        delay.tofile(delay_file, sep=';')
 
 
 if __name__ == "__main__":
     # Measure frequency response
     MODEL = 'S2V'
-    CHANNEL = 'WF'
+    CHANNEL = ['WF', 'TW']
     file_path = os.path.join(REF_DATA_PATH, MODEL)
     measure_freq_res_xlr(file_path, CHANNEL)
