e405cd4a2e3c0e3c0dd5d1ace6430ea4332aeae6
Felix Beu <felix.beu@adam-audio.de>
2024-11-25 14:59:38 +0100

    reference data measurement update

--- reference_data.py
+++ reference_data.py
@@ -34,6 +34,8 @@ S_SERIE_PRESETS = ['PURE', 'UNR', 'USER 1', 'USER 2', 'USER 3', 'USER 4', 'USER 
 CHANNELS = {'S2V': ['WF', 'TW'], 'S3V': ['WF', 'TW', 'MR'], 'S3H': ['WF', 'TW', 'MR'], 'S5V': ['WF', 'TW', 'MR'],
             'S5H': ['WF', 'TW', 'MR']}
 SHELVING_FILTERS = ['-12', '-6', '+6', '+12']
+SHELVING_CUTOFF = ['400', '5000']
+CH_DELAYS = ['0', '2.5', '5']
 
 
 def measure_freq_res_xlr(path: str, channel: list[str], preset: str):
@@ -52,9 +54,7 @@ def measure_freq_res_xlr(path: str, channel: list[str], preset: str):
     # Save curve to file
     for idx in range(len(channel)):
         name = 'Ref_Freq_Res_' + preset + '_' + channel[idx] + '_dBFS.csv'
-        delay_name = 'Ref_Delay_' + preset + '_' + channel[idx] + '_s.csv'
         file = os.path.join(path, name)
-        delay_file = os.path.join(path, delay_name)
         x_values = APx.Sequence[0]['Acoustic Response'].SequenceResults['RMS Level'].GetXValues(
             CH_IDX[idx], VerticalAxis.Left, SourceDataType.Measured, 1)
         y_values = APx.Sequence[0]['Acoustic Response'].SequenceResults['RMS Level'].GetYValues(
@@ -62,42 +62,87 @@ def measure_freq_res_xlr(path: str, channel: list[str], preset: str):
         rms_level = np.array([x_values, y_values]).T
         # Save Frequency Response to file
         np.savetxt(file, rms_level, delimiter=';')
+
+
+def measure_delay_xlr(path: str, channel: list[str], preset: str):
+    """
+
+    :param preset:
+    :param channel:
+    :type path: object
+    """
+    # Initialize APx
+    APx = APx500_Application()
+    # Load project template
+    APx.OpenProject(AP_SEQ_FREQUENCY_RESPONSE_XLR)
+    # Run APx file
+    APx.Sequence.Run()
+    # Save curve to file
+    for idx in range(len(channel)):
+        delay_name = 'Ref_Delay_' + preset + '_' + channel[idx] + '_s.csv'
+        delay_file = os.path.join(path, delay_name)
         # Save Delay to file
         delay = np.array(APx.Sequence[0]['Acoustic Response'].SequenceResults['Delay'].GetMeterValues()[idx])
         delay.tofile(delay_file, sep=';')
 
 
 if __name__ == "__main__":
-    MODEL = input('Enter the DUT model: ')
-    assert MODEL in S_SERIE_MODELS, 'Unrecognized S-Serie model'
-    PRESET = input('Enter the DUT preset: ')
-    assert PRESET in S_SERIE_PRESETS, 'Unrecognized S-Serie preset'
-    if ' ' in PRESET:
-        PRESET = PRESET.replace(' ', '_')
+    model = input('Enter the DUT model: ')
+    assert model in S_SERIE_MODELS, 'Unrecognized S-Serie model'
 
-    file_path = os.path.join(REF_DATA_PATH, MODEL)
-    measure_freq_res_xlr(file_path, CHANNELS[MODEL], PRESET)
+    file_path = os.path.join(REF_DATA_PATH, model)
 
-    measure = input('Would you like to continue the measurement process [y/n]: ')
-    assert measure in ['y', 'n'], 'Unrecognized input argument'
-    while measure != 'n':
-        PRESET = input('Enter the DUT preset: ')
-        assert PRESET in S_SERIE_PRESETS, 'Unrecognized S-Serie preset'
-        measure_freq_res_xlr(file_path, CHANNELS[MODEL], PRESET)
-        measure = input('Would you like to continue the measurement process [y/n]: ')
-        assert measure in ['yes', 'no', 'Yes', 'No', 'YES', 'NO'], 'Unrecognized input argument'
+    ref_meas = input('Would you like to proceed with the frequency response reference measurement [y/n]: ')
+    assert ref_meas in ['y', 'n'], 'Unrecognized input argument'
+    if ref_meas == 'y':
+        print('Please set the DUT preset to PURE')
+        user_preset = input('Please confirm the current DUT preset: ')
+        assert user_preset != 'PURE', 'Current user preset is not set to PURE'
+        measure_freq_res_xlr(file_path, CHANNELS[model], user_preset)
+        print('Please set the DUT preset to UNR')
+        user_preset = input('Please confirm the current DUT preset: ')
+        assert user_preset != 'UNR', 'Current user preset is not set to UNR'
+        measure_freq_res_xlr(file_path, CHANNELS[model], user_preset)
 
-    measure = input('Would you like to proceed with shelving filter measurements [y/n]: ')
-    assert measure in ['y', 'n'], 'Unrecognized input argument'
-
-    if measure == 'y':
-        PRESET = input('Enter the DUT preset: ')
-        assert PRESET in S_SERIE_PRESETS, 'Unrecognized S-Serie preset'
-        if ' ' in PRESET:
-            PRESET = PRESET.replace(' ', '_')
+    ref_meas = input('Would you like to proceed with the DUT shelving filter reference measurements [y/n]: ')
+    assert ref_meas in ['y', 'n'], 'Unrecognized input argument'
+    if ref_meas == 'y':
+        print('Please set the DUT preset to one of the custom USER presets')
+        user_preset = input('Please confirm the current DUT preset: ')
+        assert user_preset in S_SERIE_PRESETS, 'Unrecognized S-Serie preset'
+        if ' ' in user_preset:
+            user_preset = user_preset.replace(' ', '_')
+        print('Please set the DUT Lo-Shelf cutoff frequency to ' + SHELVING_CUTOFF[0] + ' Hz')
+        print('Please set the DUT Hi-Shelf cutoff frequency to ' + SHELVING_CUTOFF[1] + ' Hz')
         for i in range(len(SHELVING_FILTERS)):
-            print('Set the DUT Lo-Shelf and Hi-Shelf to ' + SHELVING_FILTERS[i] + ' dB')
+            print('Please set the DUT Lo-Shelf and Hi-Shelf to ' + SHELVING_FILTERS[i] + ' dB')
             proceed_meas = input('Confirm measurement [y/n]: ')
             if proceed_meas == 'y':
-                shelving_preset = PRESET + '_' + SHELVING_FILTERS[i] + 'dB'
-                measure_freq_res_xlr(file_path, CHANNELS[MODEL], shelving_preset)
+                shelving_preset = user_preset + '_' + SHELVING_FILTERS[i] + 'dB'
+                measure_freq_res_xlr(file_path, CHANNELS[model], shelving_preset)
+
+    ref_meas = input('Would you like to proceed with the DUT delay reference measurements [y/n]: ')
+    assert ref_meas in ['y', 'n'], 'Unrecognized input argument'
+
+    if ref_meas == 'y':
+        user_preset = input('Please enter the DUT preset: ')
+        assert user_preset in S_SERIE_PRESETS, 'Unrecognized S-Serie preset'
+        if ' ' in user_preset:
+            user_preset = user_preset.replace(' ', '_')
+        for i in range(len(CH_DELAYS)):
+            print('Please set the DUT delay to ' + CH_DELAYS[i] + ' ms')
+            proceed_meas = input('Confirm measurement [y/n]: ')
+            if proceed_meas == 'y':
+                delay_preset = user_preset + '_' + CH_DELAYS[i] + 'ms'
+                measure_delay_xlr(file_path, CHANNELS[model], delay_preset)
+
+    ref_meas = input('Would you like to measure the frequency response of additional user presets? [y/n]: ')
+    assert ref_meas in ['y', 'n'], 'Unrecognized input argument'
+    while ref_meas != 'n':
+        user_preset = input('Enter the DUT preset: ')
+        assert user_preset in S_SERIE_PRESETS, 'Unrecognized S-Serie preset'
+        measure_freq_res_xlr(file_path, CHANNELS[model], user_preset)
+        ref_meas = input('Would you like to continue the measurement process [y/n]: ')
+        assert ref_meas in ['y', 'n'], 'Unrecognized input argument'
+
+    print('Thank you very much for performing the S-Serie reference data measurements, have a nice day.')
